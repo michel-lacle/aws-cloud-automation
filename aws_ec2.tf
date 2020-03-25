dba35705eff802ea93cc7e19a516f5b60e5bcadd
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-25 18:53:28 -0400

    add key

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "aws-cloud-automation-ec2" {
   ami = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
-  
+
+  key_name = "aws-cloud-automation-ec2"
+
   tags = {
     Name = "aws-cloud-automation-ec"
     Owner = "terraform-aws-cloud-automation"
