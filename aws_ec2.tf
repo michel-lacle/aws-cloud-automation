4695c3761531806c4adf4c1f22a839cc1130810e
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-25 19:02:09 -0400

    adding security group for ssh

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,8 +1,32 @@
+resource "aws_security_group" "aws-cloud-automation-sg" {
+  name = "aws-cloud-automation-sg"
+  description = "allow ssh traffic"
+
+  vpc_id = "vpc-bb9f3bc0"
+
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "aws-cloud-automation-ec2" {
   ami = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
 
-  key_name = "aws-cloud-automation-ec2"
+  key_name = "aws-cloud-automation"
+
+  vpc_security_group_ids = [aws_security_group.aws-cloud-automation-sg.id]
 
   tags = {
     Name = "aws-cloud-automation-ec"
